d705cfcf63b251d04db8564254c83e970341a714
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2013-06-18 19:17:16 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZenAppointmentsSite::Application.config.secret_key_base = 'cf359261da832c09042f4c929ab2ed2c8b3f820c3f0ffd7ef306c08f92616960dd7d144ce1b59faf38275203474361a3e5976ee5d2d6ff7272f9fbc98859c2e3'
