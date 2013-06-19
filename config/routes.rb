81d742eac9a661ccb6d8098387152c3d387aba53
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2013-06-18 21:48:43 -0300

    testing heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ZenAppointmentsSite::Application.routes.draw do
+  root 'pages#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
