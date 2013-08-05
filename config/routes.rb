6a5899efbca6d2e7f39cb15037b31329c8e6d7fd
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2013-08-05 15:23:16 -0300

    more work on site page and gemfile update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 ZenAppointmentsSite::Application.routes.draw do
-  match '/signin', to: redirect('https://app.zenappointments.com/signin'), as: 'signin', via: :get
-  match '/signup', to: redirect('https://app.zenappointments.com/signup'), as: 'signup', via: :get
+  if Rails.env == 'development'
+    match '/signin', to: redirect('http://127.0.0.1:3000/signin'), as: 'signin', via: :get
+    match '/signup', to: redirect('http://127.0.0.1:3000/signup'), as: 'signup', via: :get
+  else
+    match '/signin', to: redirect('https://app.zenappointments.com/signin'), as: 'signin', via: :get
+    match '/signup', to: redirect('https://app.zenappointments.com/signup'), as: 'signup', via: :get
+  end
 
   get '/site', to: 'pages#site'
   root 'pages#index'
