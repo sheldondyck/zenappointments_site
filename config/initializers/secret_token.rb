2c6a1b2f34a35291c8bff3a6e8cffbf76145d79b
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2013-11-08 19:34:19 -0200

    added figaro support. index page changes

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-ZenAppointmentsSite::Application.config.secret_key_base = 'cf359261da832c09042f4c929ab2ed2c8b3f820c3f0ffd7ef306c08f92616960dd7d144ce1b59faf38275203474361a3e5976ee5d2d6ff7272f9fbc98859c2e3'
+ZenAppointmentsSite::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
